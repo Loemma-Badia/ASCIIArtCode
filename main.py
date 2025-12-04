e47f5546d73ad02d5d5ee11d5af0bb3e08a99b13
Loemma-Badia <loemma.badia@edu.esiee.fr>
2025-12-04 09:31:28 +0000

    Art Codage

--- main.py
+++ main.py
@@ -4,9 +4,7 @@
 import sys
 sys.setrecursionlimit(2000)
 
-
 #### Fonctions secondaires
-
 
 def artcode_i(s):
     """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
@@ -19,9 +17,16 @@ def artcode_i(s):
     """
     
     # votre code ici
-
-    return [ ]
-
+    LA=[]
+    occ=1
+    for letter in range(1,len(s)):
+        if s[letter]==s[letter-1]:
+            occ+=1
+        else:
+            LA.append((s[letter-1],occ))
+            occ=1
+    LA.append((s[::len(s)],occ))
+    return LA
 
 def artcode_r(s):
     """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
@@ -34,20 +39,26 @@ def artcode_r(s):
     """
     
     # votre code ici
-
     # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    if s=='':
+        return []
 
-    return []
-    
+    # recherche nombre de caractères identiques au premier
+
+    ref = s[0]
+    i= 1
+    while i < len(s) and s[i] == ref:
+        i+=1
+
+    return [(ref, i)] + artcode_r(s[i:])
 
 #### Fonction principale
 
-
 def main():
-    print(artcode_i('MMMMaaacXolloMM'))
-    print(artcode_r('MMMMaaacXolloMM'))
+    print(artcode_i('gggfhhhhhhggg'))
+    print(artcode_r('~~~~~@@@---@@@~~~~~'))
+#[('g', 3), ('f', 1), ('h', 6), ('g', 3)]
+#[('~', 5), ('@', 3), ('-', 3), ('@', 3), ('~', 5)]
 
 if __name__ == "__main__":
     main()
